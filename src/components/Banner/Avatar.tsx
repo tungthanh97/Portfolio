bcc942296d92a58c192688f8f7547036758b291d
tung <tung020197@gmail.com>
2023-09-26 22:59:56 +0700

    refactor banner avatar

---
+++ src/components/Banner/Avatar.tsx
@@ -0,0 +1,27 @@
+import Image from 'next/image';
+import AvatarBackground from './AvatarBackground';
+import classNames from '@package/lib/utils/classNames';
+import { TClassName } from '@package/lib/types/components';
+
+const Avatar = ({ wrapperClassName }: { wrapperClassName: TClassName }) => {
+    return (
+        <figure className={classNames('relative', wrapperClassName)}>
+            <div className="absolute -right-36 -top-1/4  max-w-max">
+                <AvatarBackground className="fill-custom-primary" />
+            </div>
+            <picture className="dark:border-grey-100 relative z-10 block h-96 w-96 border border-primary-black bg-blend-difference ">
+                <Image
+                    alt="My avatar"
+                    src="/images/avatar.png"
+                    fill={true}
+                    priority={false}
+                    placeholder="empty"
+                    sizes='width="24rem" height="24rem"'
+                    className="absolute !-left-8 !-top-10 items-center object-cover"
+                />
+            </picture>
+        </figure>
+    );
+};
+
+export default Avatar;
--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image';
 import AvatarBackground from './AvatarBackground';
+import Contact from '@components/Contact';
+import Avatar from './Avatar';
 
 const Banner = () => {
     return (
         <section className="h-screen w-full">
-            <div className="center-row h-full bg-white dark:bg-primary-black">
+            <div className="center-row bg-primary-white h-full dark:bg-primary-black">
                 <div className="my-[20vh] flex h-max w-full items-center justify-evenly lg:my-60 lg:h-screen">
                     <article className="lg:max-w-[30vw]">
                         <h1 className="pr-4 text-7xl text-custom-primary">Frontend Developer.</h1>
@@ -13,42 +15,14 @@ const Banner = () => {
                             experiences.
                         </p>
                     </article>
-                    <figure className="relative hidden h-max w-max lg:block">
-                        <div className="absolute -right-36 -top-1/4 max-w-max">
-                            <AvatarBackground className="fill-custom-primary" />
-                        </div>
-                        <picture className="dark:border-grey-100 relative z-10 block h-96 w-96 border border-primary-black bg-blend-difference">
-                            <Image
-                                alt="My avatar"
-                                src="/images/avatar.png"
-                                fill={true}
-                                className="absolute !-left-8 !-top-10 items-center object-cover"
-                            />
-                        </picture>
-                    </figure>
+                    <Avatar wrapperClassName="hidden h-max w-max lg:block" />
                 </div>
             </div>
-            <div className="flex items-center justify-center bg-white p-36 dark:bg-primary-black lg:hidden">
-                <figure className="relative h-max w-max">
-                    <div className="absolute -right-36 -top-1/4  max-w-max">
-                        <AvatarBackground className="fill-custom-primary" />
-                    </div>
-                    {/* <Image
-                        alt="avatar-background"
-                        src="/images/avatar-bg.svg"
-                        width={701}
-                        height={935}
-                        className="absolute -right-36 -top-1/4 max-w-max"
-                    /> */}
-                    <picture className="dark:border-grey-100 relative z-10 block h-96 w-96 border border-primary-black bg-blend-difference ">
-                        <Image
-                            alt="My avatar"
-                            src="/images/avatar.png"
-                            fill={true}
-                            className="absolute !-left-8 !-top-10 items-center object-cover"
-                        />
-                    </picture>
-                </figure>
+            <div className="bg-primary-white flex items-center justify-center p-36 dark:bg-primary-black lg:hidden">
+                <Avatar wrapperClassName="h-max w-max" />
+            </div>
+            <div className="center-row fixed bottom-4 z-50 w-full">
+                <Contact />
             </div>
         </section>
     );
