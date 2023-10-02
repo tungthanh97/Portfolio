f8ca7c5760de70d535b77ec680ff3bca5e935935
tung <tung020197@gmail.com>
2023-10-02 22:45:52 +0700

    change bio text color on light mode

--- src/components/Banner/BioDescription.tsx
+++ src/components/Banner/BioDescription.tsx
@@ -2,7 +2,7 @@ const BioDescription = () => {
     return (
         <article className="z-10 lg:max-w-[30vw]">
             <h1 className="pr-4 text-7xl text-custom-primary">Frontend Developer.</h1>
-            <p className="my-6 text-xl leading-relaxed text-white mix-blend-difference">
+            <p className="my-6 text-xl leading-relaxed text-black dark:text-white">
                 I like to craft solid and scalable frontend products with great user experiences.
             </p>
         </article>
