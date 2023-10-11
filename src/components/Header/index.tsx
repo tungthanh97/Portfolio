46af956fad1eb8c55a242cdf9ee81c72e5359884
tung <tung020197@gmail.com>
2023-10-11 18:41:58 +0700

    center header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -18,18 +18,22 @@ const PageHeader = () => {
 
     return (
         <section className="absolute top-4 z-20 mx-auto flex w-full px-4 lg:px-10">
-            <div className="flex w-full justify-between px-4 py-6">
-                <div
-                    className={classNames(
-                        'pl- relative z-[99999] flex items-center py-1 text-custom-primary'
-                    )}
-                >
-                    <Logo />
+            <div className="grid w-full grid-cols-2 grid-rows-1 px-4 py-6 lg:grid-cols-3">
+                <div className="flex flex-row items-center justify-start gap-4 md:gap-6 lg:gap-14">
+                    <div
+                        className={classNames(
+                            'pl- relative z-[99999] flex items-center py-1 text-custom-primary'
+                        )}
+                    >
+                        <Logo />
+                    </div>
                 </div>
 
-                <NavBar navBarContent={navBarContent} />
+                <div className="center hidden w-max lg:block">
+                    <NavBar navBarContent={navBarContent} />
+                </div>
 
-                <div className="flex w-32 flex-row items-center justify-end gap-4 md:gap-6 lg:gap-14">
+                <div className="flex flex-row items-center justify-end gap-4 md:gap-6 lg:gap-14">
                     <ThemeToggle theme={theme} onClick={() => toggleTheme()} />{' '}
                     <div className={classNames('lg:center-row hidden h-full')}>
                         <RollingBox />
